81fae4abbd661b7ff701bc3c9889f53e155756de
juliusneria <julius@dotweblabs.com>
2017-08-25 00:51:17 +0800

    ADMIN ABOUT, FACILITY, ACTIVITY add edit delete function

--- development/app/app/app.module.js
+++ development/app/app/app.module.js
@@ -4,4 +4,6 @@ angular
 myFunction.$inject = ['$localStorage','$rootScope'];
 function myFunction($localStorage,$rootScope){
 
+    Parse.initialize($rootScope.parserServer.appId, $rootScope.parserServer.javascriptKey);
+    Parse.serverURL = $rootScope.parserServer.url;
 }
--- development/app/app/components/controllers/bookCtrl.js
+++ development/app/app/components/controllers/bookCtrl.js
@@ -4,4 +4,25 @@ angular
 bookCtrl.$inject = ['$scope','$timeout','$state','$localStorage'];
 function bookCtrl($scope, $timeout, $state,$localStorage) {
 
+    $scope.startdate = null;
+    $scope.enddate = null;
+
+    var monthpicker = new MaterialDatepicker('#startDate', {
+        lang: 'en',
+        orientation: 'landscape',
+        theme: 'light',
+        color: '#726277'
+    });
+
+    var monthpicker = new MaterialDatepicker('#endDate', {
+        lang: 'en',
+        orientation: 'landscape',
+        theme: 'light',
+        color: '#726277'
+    });
+
+    $scope.submit = function(){
+        console.log($scope.startdate, $scope.enddate);
+    }
+
 }
